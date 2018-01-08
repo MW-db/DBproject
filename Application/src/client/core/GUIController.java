90cb04c7ffa7f4840cf1cbaeae98aa8f4abb5d01
Wojtek Zaniewski <wojtek.z@interia.eu>
2018-01-09 00:45:17 +0100

    Added client view, change all list into table, add table and column initialization and filling clients and workers table after add button clicked

--- Application/src/client/core/GUIController.java
+++ Application/src/client/core/GUIController.java
@@ -1,10 +1,15 @@
 package client.core;
 
 import javafx.application.Platform;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.StackPane;
+
+import java.util.ArrayList;
 
 public class GUIController {
 
@@ -63,15 +68,25 @@ public class GUIController {
     @FXML
     private Button ownerExitButton;
     @FXML
-    private ListView clientsTable;
+    private TableView<String> clientsTable;
     @FXML
-    private ListView workersTable;
+    private TableView<String> workersTable;
     @FXML
-    private ListView infoTable;
+    private TableView<String> infoTable;
     @FXML
-    private ListView deliveryWaitingTable;
+    private TableView<String> deliveryWaitingTable;
     @FXML
-    private ListView deliveryIncomingTable;
+    private TableView<String> deliveryIncomingTable;
+    @FXML
+    private TableColumn<String, String> workersColumn;
+    @FXML
+    private TableColumn<String, String> deliveryWaitingColumn;
+    @FXML
+    private TableColumn<String, String> clientsColumn;
+    @FXML
+    private TableColumn<String, String> deliveryIncomingColumn;
+    @FXML
+    private TableColumn<String, String> infoColumn;
 
     //---------WORKER PAGE-------------
 
@@ -102,11 +117,137 @@ public class GUIController {
     @FXML
     private Button workerCreateDeliveryButton;
     @FXML
-    private ListView clientsOrderListTable;
+    private TableView<String> clientsOrderListTable;
     @FXML
-    private ListView waitingDeliveryTable;
+    private TableView<String> waitingDeliveryTable;
     @FXML
-    private ListView goingDeliveryTable;
+    private TableView<String> goingDeliveryTable;
+    @FXML
+    private TableColumn<String, String> clientsOrderListColumn;
+    @FXML
+    private TableColumn<String, String> waitingDeliveryColumn;
+    @FXML
+    private TableColumn<String, String> goingDeliveryColumn;
+
+
+
+    //---------CLIENT PAGE-------------
+
+    @FXML
+    private StackPane clientPage;
+    @FXML
+    private Label clientDateLabel;
+    @FXML
+    private Label summaryLabel;
+    @FXML
+    private Label walletLabel;
+    @FXML
+    private Button filtrButton;
+    @FXML
+    private Button clientRefreshButton;
+    @FXML
+    private Button payButton;
+    @FXML
+    private Button addBalanceButton;
+    @FXML
+    private Button addProductButton;
+    @FXML
+    private Button removeProductButton;
+    @FXML
+    private Button cancelOrderButton;
+    @FXML
+    private Button checkPreviousTransactionButton;
+    @FXML
+    private Button clientExitButton;
+    @FXML
+    private TableView<String> productsListTable;
+    @FXML
+    private TableView<String> cartTable;
+    @FXML
+    private TableView<String> previousTransactionTable;
+    @FXML
+    private TableColumn<String, String> productsListColumn;
+    @FXML
+    private TableColumn<String, String> cartColumn;
+    @FXML
+    private TableColumn<String, String> previousTransactionColumn;
+    @FXML
+    private CheckBox foodCheckBox;
+    @FXML
+    private CheckBox drinksCheckBox;
+    @FXML
+    private CheckBox otherCheckBox;
+
+
+    //====== OWNER OBSERVABLE =======
+    private ObservableList<String> workers;
+    private ObservableList<String> clients;
+    private ObservableList<String> deliveryInc;
+    private ObservableList<String> deliveryWait;
+    private ObservableList<String> info;
+
+    //====== WORKER OBSERVABLE =======
+    private ObservableList<String> workerClOrder;
+    private ObservableList<String> workerDeliveryInc;
+    private ObservableList<String> workerDeliveryWait;
+
+    //====== CLIENT OBSERVABLE =======
+    private ObservableList<String> prevTrans;
+    private ObservableList<String> productList;
+    private ObservableList<String> cart;
+
+
+    @FXML
+    void initialize() {
+        //OWNER TABLE
+        clients = FXCollections.observableArrayList();
+        clientsColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        clientsTable.setItems(clients);
+
+        workers = FXCollections.observableArrayList();
+        workersColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        workersTable.setItems(workers);
+
+        deliveryWait = FXCollections.observableArrayList();
+        deliveryWaitingColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        deliveryWaitingTable.setItems(deliveryWait);
+
+        deliveryInc = FXCollections.observableArrayList();
+        deliveryIncomingColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        deliveryIncomingTable.setItems(deliveryInc);
+
+        info = FXCollections.observableArrayList();
+        infoColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        infoTable.setItems(info);
+
+
+        //WORKER TABLE
+        workerClOrder = FXCollections.observableArrayList();
+        clientsOrderListColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        clientsOrderListTable.setItems(workerClOrder);
+
+        workerDeliveryInc = FXCollections.observableArrayList();
+        goingDeliveryColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        goingDeliveryTable.setItems(workerDeliveryInc);
+
+        workerDeliveryWait = FXCollections.observableArrayList();
+        waitingDeliveryColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        waitingDeliveryTable.setItems(workerDeliveryWait);
+
+
+        //CLIENT TABLE
+        prevTrans = FXCollections.observableArrayList();
+        previousTransactionColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        previousTransactionTable.setItems(prevTrans);
+
+        productList = FXCollections.observableArrayList();
+        productsListColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        productsListTable.setItems(productList);
+
+        cart = FXCollections.observableArrayList();
+        cartColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        cartTable.setItems(cart);
+    }
 
 
 
@@ -138,7 +279,11 @@ public class GUIController {
     //========= OWNER ACTIONS =============
 
     public void addWorkerOnClick(ActionEvent event) {
-
+        String[] list = {"worker1", "worker2", "worker3", "worker4", "worker5"};
+        run(() -> {
+            workers.setAll(list);
+            workersTable.setItems(workers);
+        });
     }
 
     public void removeWorkerOnClick(ActionEvent event) {
@@ -146,7 +291,11 @@ public class GUIController {
     }
 
     public void addClientOnClick(ActionEvent event) {
-
+        String[] list = {"client1", "client2", "client13"};
+        run(() -> {
+            clients.setAll(list);
+            clientsTable.setItems(clients);
+        });
     }
 
     public void removeClientOnClick(ActionEvent event) {
@@ -182,6 +331,9 @@ public class GUIController {
     }
 
 
+
+
+
     //========= WORKER ACTIONS =============
 
     public void workerAcceptOrderOnClick(ActionEvent event) {
@@ -198,5 +350,68 @@ public class GUIController {
 
     public void workerCreateDeliveryOnClick(ActionEvent event) {
 
+    }
+
+
+
+
+    //========= CLIENT ACTIONS =============
+
+    public void filtrOnClick(ActionEvent event) {
+
+    }
+
+    public void clientRefreshOnClick(ActionEvent event) {
+
+    }
+
+    public void payOnClick(ActionEvent event) {
+
+    }
+
+    public void addBalanceOnClick(ActionEvent event) {
+
+    }
+
+    public void addProductOnClick(ActionEvent event) {
+
+    }
+
+    public void removeProductOnClick(ActionEvent event) {
+
+    }
+
+    public void cancelOrderOnClick(ActionEvent event) {
+
+    }
+
+    public void checkPreviousTransactionOnClick(ActionEvent event) {
+
+    }
+
+    public void clientExitButton(ActionEvent event) {
+
+    }
+
+    public void changeFoodFiltrOnAction(ActionEvent event) {
+
+    }
+
+    public void changeDrinkFiltrOnAction(ActionEvent event) {
+
+    }
+
+    public void changeOtherFiltrOnAction(ActionEvent event) {
+
+    }
+
+
+
+    //OTHERS------------------------------------------------
+
+    public static void run(Runnable treatment) {
+        if(treatment == null) throw new IllegalArgumentException("The treatment to perform can not be null");
+        if(Platform.isFxApplicationThread()) treatment.run();
+        else Platform.runLater(treatment);
     }
 }
